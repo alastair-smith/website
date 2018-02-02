96a348b28b0700a833a0213a9ce0ccecf3e0f89f
Alastair Smith <al.smith.dev@gmail.com>
2018-02-02 21:48:29 +0000

    made hex link component

--- HexLink/HexLink.js
+++ HexLink/HexLink.js
@@ -1,6 +1,4 @@
 const currentDocument = document.currentScript.ownerDocument
-// const style = currentDocument.querySelector('style')
-// document.head.appendChild(style)
 
 class HexLink extends HTMLElement {
   connectedCallback () {
@@ -11,19 +9,24 @@ class HexLink extends HTMLElement {
 
     const title = this.getAttribute('title')
     const url = this.getAttribute('url')
+    const height = this.getAttribute('hex-height')
     this.render({
       title,
-      url
+      url,
+      height
     })
   }
 
-  render ({title, url}) {
+  render ({title, url, height}) {
+    const points = '300,150 225,280 75,280 0,150 75,20 225,20'
     // Fill the respective areas of the card using DOM manipulation APIs
     // All of our components elements reside under shadow dom. So we created a this.shadowRoot property
     // We use this property to call selectors so that the DOM is searched only under this subtree
-    this.shadowRoot.querySelector('.link').href = url
-    this.shadowRoot.querySelector('.title').innerHTML =
-      `<h1>${title}</h1>`
+    const containerHex = this.shadowRoot.querySelector('.hex-container')
+    containerHex.setAttribute('style', `height:${height};`)
+    this.shadowRoot.querySelector('.link').setAttribute('href', url)
+    this.shadowRoot.querySelectorAll('.hex')
+      .forEach(element => element.setAttribute('points', points))
   }
 }
 
