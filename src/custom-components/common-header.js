7b6139dc867ab2c2169a90de1dc5eabf86d1b83a
Alastair Smith <al.smith.dev@gmail.com>
2019-05-09 23:12:14 +0100

    convert header to vanilla web component

--- src/custom-components/common-header.js
+++ src/custom-components/common-header.js
@@ -1,45 +1,38 @@
-import { LitElement, html, css } from 'lit-element'
-import './common-navigation'
+const WEBSITE_NAME = 'alsmith'
 
-export class CommonHeader extends LitElement {
-  constructor () {
-    super()
-
-    this.name = 'home'
-  }
-
-  static get properties () {
-    return {
-      name: { type: String }
+const styleTemplate = document.createElement('template')
+styleTemplate.innerHTML = `
+  <style>
+    header {
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+      justify-content: space-between;
+      box-shadow: 0px 2px 3px 0px rgba(0,0,0,0.1);
+      padding: 30px;
     }
+  
+    a {
+      text-decoration: none;
+      color: #5c5e73;
+      font-size: 30px;
+    }
+  </style>
+`
+
+let headerTemplate = document.createElement('template')
+headerTemplate.innerHTML = `
+  <header>
+    <a href='/'>${WEBSITE_NAME}</a>
+  </header>
+`
+
+customElements.define('common-header', class extends HTMLElement {
+  constructor (...args) {
+    super(...args)
+
+    const shadowRoot = this.attachShadow({ mode: 'open' })
+    shadowRoot.appendChild(styleTemplate.content.cloneNode(true))
+    shadowRoot.appendChild(headerTemplate.content.cloneNode(true))
   }
-
-  static get styles () {
-    return css`
-      header {
-        display: flex;
-        flex-direction: row;
-        align-items: center;
-        justify-content: space-between;
-        box-shadow: 0px 2px 3px 0px rgba(0,0,0,0.1);
-        padding: 30px;
-      }
-
-      a {
-        text-decoration: none;
-        color: #5c5e73;
-        font-size: 30px;
-      }
-    `
-  }
-
-  render () {
-    return html`
-      <header>
-        <a href='/'>${this.name}</a>
-      </header>
-    `
-  }
-}
-
-customElements.define('common-header', CommonHeader)
+})
