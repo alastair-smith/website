72e6c550208442cbfbc2ba83f450603ad3f90569
Alastair Smith <al.smith.dev@gmail.com>
2019-07-02 22:39:37 +0100

    use old cached versions for optional resources

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,26 +1,80 @@
 /* eslint-env serviceworker */
 
-const CURRENT_CACHE_NAME = `alsmith.dev-service-worker-1`
+const LATEST_CACHE_NAME = 'general-service-worker-v1'
+const OLD_CONTENT_CACHE_NAME = 'backup-cache-v1'
 const OFFLINE_URL = '/offline.html'
 
-const contentToCache = [
+const mandatoryCacheList = [
   '/',
   '/styles.css',
-  '/offline.html'
+  '/offline.html',
+  '/custom-components/standard-page-wrapper.js'
 ]
 
-const cacheResources = async () => {
-  const cache = await caches.open(CURRENT_CACHE_NAME)
-  return cache.addAll(contentToCache)
+const optionalCacheList = [
+  '/assets/images/alastair.260px.jpg',
+  '/assets/images/alastair.260px.webp',
+  '/assets/images/alastair.343px.jpg',
+  '/assets/images/alastair.343px.webp',
+  '/android-chrome-192x192.png',
+  '/android-chrome-512x512.png',
+  '/apple-touch-icon.png',
+  '/browserconfig.xml',
+  '/favicon-16x16.png',
+  '/favicon-32x32.png',
+  '/favicon.ico',
+  '/mstile-150x150.png',
+  '/safari-pinned-tab.svg',
+  '/site.webmanifest'
+]
+
+const addMandatoryItemsToCache = async () => {
+  const cache = await caches.open(LATEST_CACHE_NAME)
+  return cache.addAll(mandatoryCacheList)
+}
+
+const getFromLatestCache = async request => {
+  const cache = await caches.open(LATEST_CACHE_NAME)
+  return cache.match(request)
+}
+
+const addToNewCache = async request => {
+  try {
+    const latestCache = await caches.open(LATEST_CACHE_NAME)
+    await latestCache.add(request)
+  } catch (error) {
+    console.log(`Failed to update new cache in background for ${request.url}`)
+  }
+}
+
+const getFromOldCacheAndUpdateCache = async request => {
+  const oldCache = await caches.open(OLD_CONTENT_CACHE_NAME)
+  const oldResponse = await oldCache.match(request)
+
+  if (oldResponse) addToNewCache(request)
+
+  return oldResponse
+}
+
+const fetchAndUpdateCache = async request => {
+  let response
+  try {
+    const cache = await caches.open(LATEST_CACHE_NAME)
+    response = await fetch(request)
+    cache.put(request, response.clone())
+  } catch (error) {
+    response = undefined
+  }
+  return response
 }
 
 const getResource = async request => {
   let response
   try {
-    const cachedValue = await caches.match(request)
-    response = cachedValue || await fetch(request)
+    response = await getFromLatestCache(request) ||
+      await getFromOldCacheAndUpdateCache(request) ||
+      await fetchAndUpdateCache(request)
   } catch (error) {
-    console.error('error', error, request.url)
     response = (
       request.mode === 'navigate' ||
       (
@@ -31,17 +85,33 @@ const getResource = async request => {
       ? await caches.match(OFFLINE_URL)
       : undefined
   }
+
   return response
 }
 
 const deleteOldCaches = async () => {
-  const cacheKeys = await caches.keys()
-  const oldCacheKeys = cacheKeys.filter(key => key !== CURRENT_CACHE_NAME)
-  return Promise.all(oldCacheKeys.map(key => caches.delete(key)))
+  // backup most recent versions of optional content
+  const oldContentCache = await caches.open(OLD_CONTENT_CACHE_NAME)
+  await Promise.all(
+    optionalCacheList
+      .map(async request => {
+        const latestResponse = await caches.match(request)
+        if (latestResponse) await oldContentCache.put(request, latestResponse)
+      }))
+
+  // delete old caches
+  await Promise.all(
+    (await caches.keys())
+      .filter(key =>
+        key !== LATEST_CACHE_NAME &
+        key !== OLD_CONTENT_CACHE_NAME
+      )
+      .map(key => caches.delete(key))
+  )
 }
 
 self.addEventListener('install', event =>
-  event.waitUntil(cacheResources())
+  event.waitUntil(addMandatoryItemsToCache())
 )
 
 self.addEventListener('fetch', event =>
