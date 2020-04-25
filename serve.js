a23c84828c5dd4d04884699040a17d5ed36fedb9
Alastair Smith <al.smith.dev@gmail.com>
2020-04-25 21:34:26 +0100

    changed message on launch

--- serve.js
+++ serve.js
@@ -2,10 +2,11 @@
 
 const express = require('express')
 
+const port = 8080
 const app = express()
 
 app.use(express.static('src'))
 
-app.listen(8080, () => {
-  console.log('listening')
+app.listen(port, () => {
+  console.log(`listening on port ${port}`)
 })
