e38f3926e5fbaff4a1dabfbf6dfa27e4f7e2bec5
Alastair Smith <al.smith.dev@gmail.com>
2021-04-08 22:31:41 +0100

    set terraform required providers config

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -3,6 +3,18 @@ terraform {
     key    = "website.json"
     region = "eu-west-1"
   }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "~> 2.0"
+    }
+  }
 }
 
 provider "aws" {
