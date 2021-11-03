dff0b3c4ff64287fa3aec146da4d0daa142e35de
Alastair Smith <al.smith.dev@gmail.com>
2021-11-03 21:17:42 +0000

    added api gateway for kelly lambda function

--- infrastructure/modules/kelly_endpoint/main.tf
+++ infrastructure/modules/kelly_endpoint/main.tf
@@ -72,3 +72,62 @@ resource "aws_lambda_layer_version" "kelly_dependencies" {
   s3_bucket                = var.package_bucket
   s3_key                   = var.kelly_layer_key
 }
+
+resource "aws_apigatewayv2_api" "lambda" {
+  name          = local.name_prefix
+  protocol_type = "HTTP"
+}
+
+resource "aws_apigatewayv2_stage" "lambda" {
+  api_id = aws_apigatewayv2_api.lambda.id
+
+  name        = "kelly-lambda-stage"
+  auto_deploy = true
+
+  access_log_settings {
+    destination_arn = aws_cloudwatch_log_group.api_gateway.arn
+
+    format = jsonencode({
+      requestId               = "$context.requestId"
+      sourceIp                = "$context.identity.sourceIp"
+      requestTime             = "$context.requestTime"
+      protocol                = "$context.protocol"
+      httpMethod              = "$context.httpMethod"
+      resourcePath            = "$context.resourcePath"
+      routeKey                = "$context.routeKey"
+      status                  = "$context.status"
+      responseLength          = "$context.responseLength"
+      integrationErrorMessage = "$context.integrationErrorMessage"
+      }
+    )
+  }
+}
+
+resource "aws_apigatewayv2_integration" "lambda" {
+  api_id = aws_apigatewayv2_api.lambda.id
+
+  integration_uri    = aws_lambda_function.kelly.invoke_arn
+  integration_type   = "AWS_PROXY"
+  integration_method = "POST"
+}
+
+resource "aws_apigatewayv2_route" "kelly" {
+  api_id = aws_apigatewayv2_api.lambda.id
+
+  route_key = "GET /"
+  target    = "integrations/${aws_apigatewayv2_integration.lambda.id}"
+}
+
+resource "aws_cloudwatch_log_group" "api_gateway" {
+  name = "/aws/api_gw/${aws_apigatewayv2_api.lambda.name}"
+}
+
+resource "aws_lambda_permission" "api_gateway" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.kelly.function_name
+  principal     = "apigateway.amazonaws.com"
+
+  source_arn = "${aws_apigatewayv2_api.lambda.execution_arn}/*/*"
+}
+
