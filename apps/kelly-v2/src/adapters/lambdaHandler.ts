04ae0b8a9b53e4ea8951eeb339ec60d15d0761f2
alastair-smith <contact@alsmith.dev>
2024-05-31 20:41:02 +0100

    fix eslint

--- apps/kelly-v2/src/adapters/lambdaHandler.ts
+++ apps/kelly-v2/src/adapters/lambdaHandler.ts
@@ -1,7 +1,22 @@
 import type { Handler } from 'aws-lambda';
+import { z } from 'zod';
 
 import kelly from '@/lib/kelly';
 
-export const handler: Handler<unknown, string> = (event) => {
-  return kelly('x');
+const schema = z.object({
+  text: z.string(),
+});
+
+const validateInput = (event: unknown): Parameters<typeof kelly> => {
+  const parsed = schema.parse(event);
+
+  return [parsed.text];
+};
+
+export const handler: Handler<unknown, string> = async (event) => {
+  const input = validateInput(event);
+
+  const result = await kelly(...input);
+
+  return result;
 };
---
+++ apps/kelly-v2/src/index.ts
@@ -0,0 +1,1 @@
+export { handler } from './adapters/lambdaHandler';
