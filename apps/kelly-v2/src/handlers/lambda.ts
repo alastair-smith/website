349b5a095074642c72e1d1dd2da255eacdbf5aab
alastair-smith <contact@alsmith.dev>
2024-06-01 17:02:02 +0100

    restructure lambda handler

--- apps/kelly-v2/src/handlers/lambda.ts
+++ apps/kelly-v2/src/handlers/lambda.ts
@@ -7,6 +7,7 @@ import { z } from 'zod';
 
 import generateGif from '@/lib/generateGif';
 
+// inputs
 const schema = z.object({
   queryStringParameters: z.object({
     text: z.string().min(1),
@@ -19,42 +20,42 @@ const validateInput = (event: unknown): Parameters<typeof generateGif> => {
   return [parsed.queryStringParameters.text];
 };
 
+// outputs
+const redirectResponse: APIGatewayProxyResult = {
+  statusCode: 302,
+  headers: {
+    Location: 'https://alsmith.dev/kelly',
+  },
+  isBase64Encoded: false,
+  body: '',
+} as const;
+const serverErrorResponse: APIGatewayProxyResult = {
+  statusCode: 500,
+  isBase64Encoded: false,
+  body: 'INTERNAL SERVER ERROR',
+} as const;
+const getSuccessResponse = (body: string): APIGatewayProxyResult => ({
+  statusCode: 200,
+  headers: {
+    'Content-Type': 'image/gif',
+  },
+  body,
+  isBase64Encoded: true,
+});
+
 export const handler: Handler<
   APIGatewayProxyEvent,
   APIGatewayProxyResult
 > = async (event) => {
-  let input;
   try {
-    input = validateInput(event);
-  } catch (error) {
-    return {
-      statusCode: 302,
-      headers: {
-        Location: 'https://alsmith.dev/kelly',
-      },
-      isBase64Encoded: false,
-      body: '',
-    };
-  }
+    const input = validateInput(event);
 
-  let result;
-  try {
-    result = await generateGif(...input);
+    const result = await generateGif(...input);
+
+    return getSuccessResponse(result);
   } catch (error) {
     console.error(error);
-    return {
-      statusCode: 500,
-      isBase64Encoded: false,
-      body: 'INTERNAL SERVER ERROR',
-    };
-  }
 
-  return {
-    statusCode: 200,
-    headers: {
-      'Content-Type': 'image/gif',
-    },
-    body: result,
-    isBase64Encoded: true,
-  };
+    return error instanceof z.ZodError ? redirectResponse : serverErrorResponse;
+  }
 };
