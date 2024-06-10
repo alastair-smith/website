a5cc3bf36341a1f2dea5f839cf0af14a99353d9c
alastair-smith <contact@alsmith.dev>
2024-06-10 22:12:03 +0100

    update url for kelly api

--- apps/frontend/src/app/api/kelly/route.ts
+++ apps/frontend/src/app/api/kelly/route.ts
@@ -6,7 +6,7 @@ export const runtime = 'edge';
 export async function GET(request: NextRequest) {
   const { search: query } = new URL(request.url);
   const res = await fetch(
-    `https://rjvxmktbli.execute-api.eu-west-1.amazonaws.com/kelly-lambda-stage/${query}`
+    `https://csdmeeplo0.execute-api.eu-west-1.amazonaws.com/${query}`
   );
 
   return res.headers.get('content-type') === 'image/png' ||
