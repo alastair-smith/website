0548749511e363b292a8150ced009dc333bbd7fa
alastair-smith <contact@alsmith.dev>
2024-08-28 12:33:23 +0100

    fix build command

--- apps/frontend/scripts/deploy.sh
+++ apps/frontend/scripts/deploy.sh
@@ -4,7 +4,6 @@ set -eou pipefail
 
 pnpm wrangler pages deploy .vercel/output/static \
   --project-name website-frontend \
-  --custom-entrypoint=./src/cloudflare-entrypoint.ts \
   --var "GIT_BRANCH_NAME:$GIT_BRANCH_NAME" \
   --var "ENVIRONMENT:$ENVIRONMENT" \
   --var "NEW_RELIC_LICENSE_KEY:$NEW_RELIC_LICENSE_KEY" \
